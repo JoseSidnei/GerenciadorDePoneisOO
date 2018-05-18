ecbf7136e9415cd111b13e2309c8b639f72158a2
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-05-18 17:28:42 -0300

    Initial commit

---
+++ GerenciadorDePoneisOO/CadastroDeFofuras.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace GerenciadorDePoneisOO
+{
+    partial class CadastroDeFofuras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApelido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbRaca = new System.Windows.Forms.ComboBox();
+            this.tbNivelFofura = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbCorCrista = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbVivo = new System.Windows.Forms.RadioButton();
+            this.rbMorto = new System.Windows.Forms.RadioButton();
+            this.cbDancarino = new System.Windows.Forms.CheckBox();
+            this.cbLocalidade = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtDescricao = new System.Windows.Forms.RichTextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dgvPoneys = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tbNivelFofura)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPoneys)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Apelido";
+            // 
+            // txtApelido
+            // 
+            this.txtApelido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApelido.Location = new System.Drawing.Point(167, 23);
+            this.txtApelido.Name = "txtApelido";
+            this.txtApelido.Size = new System.Drawing.Size(285, 22);
+            this.txtApelido.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Raça";
+            // 
+            // cbRaca
+            // 
+            this.cbRaca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbRaca.FormattingEnabled = true;
+            this.cbRaca.Items.AddRange(new object[] {
+            "Fofo nível Estagiário",
+            "Fofo nível Master",
+            "Fofo nível Senior",
+            "Fofo nível Pleno"});
+            this.cbRaca.Location = new System.Drawing.Point(167, 83);
+            this.cbRaca.Name = "cbRaca";
+            this.cbRaca.Size = new System.Drawing.Size(285, 24);
+            this.cbRaca.TabIndex = 3;
+            // 
+            // tbNivelFofura
+            // 
+            this.tbNivelFofura.Location = new System.Drawing.Point(167, 132);
+            this.tbNivelFofura.Name = "tbNivelFofura";
+            this.tbNivelFofura.Size = new System.Drawing.Size(285, 45);
+            this.tbNivelFofura.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nível de Fofura";
+            // 
+            // cbCorCrista
+            // 
+            this.cbCorCrista.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCorCrista.FormattingEnabled = true;
+            this.cbCorCrista.Items.AddRange(new object[] {
+            "Rosê",
+            "Pink",
+            "Fuschia",
+            "Magenta"});
+            this.cbCorCrista.Location = new System.Drawing.Point(167, 207);
+            this.cbCorCrista.Name = "cbCorCrista";
+            this.cbCorCrista.Size = new System.Drawing.Size(285, 24);
+            this.cbCorCrista.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(70, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cor Crista";
+            // 
+            // rbVivo
+            // 
+            this.rbVivo.AutoSize = true;
+            this.rbVivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbVivo.Location = new System.Drawing.Point(167, 237);
+            this.rbVivo.Name = "rbVivo";
+            this.rbVivo.Size = new System.Drawing.Size(61, 24);
+            this.rbVivo.TabIndex = 8;
+            this.rbVivo.TabStop = true;
+            this.rbVivo.Text = "Vivo";
+            this.rbVivo.UseVisualStyleBackColor = true;
+            // 
+            // rbMorto
+            // 
+            this.rbMorto.AutoSize = true;
+            this.rbMorto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMorto.Location = new System.Drawing.Point(362, 236);
+            this.rbMorto.Name = "rbMorto";
+            this.rbMorto.Size = new System.Drawing.Size(73, 24);
+            this.rbMorto.TabIndex = 9;
+            this.rbMorto.TabStop = true;
+            this.rbMorto.Text = "Morto";
+            this.rbMorto.UseVisualStyleBackColor = true;
+            // 
+            // cbDancarino
+            // 
+            this.cbDancarino.AutoSize = true;
+            this.cbDancarino.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDancarino.Location = new System.Drawing.Point(246, 237);
+            this.cbDancarino.Name = "cbDancarino";
+            this.cbDancarino.Size = new System.Drawing.Size(110, 24);
+            this.cbDancarino.TabIndex = 10;
+            this.cbDancarino.Text = "Dançarino";
+            this.cbDancarino.UseVisualStyleBackColor = true;
+            // 
+            // cbLocalidade
+            // 
+            this.cbLocalidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbLocalidade.FormattingEnabled = true;
+            this.cbLocalidade.Items.AddRange(new object[] {
+            "Poneyslandia",
+            "PoneyCity",
+            "MetroPoney",
+            "NovaPoney"});
+            this.cbLocalidade.Location = new System.Drawing.Point(167, 295);
+            this.cbLocalidade.Name = "cbLocalidade";
+            this.cbLocalidade.Size = new System.Drawing.Size(285, 24);
+            this.cbLocalidade.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(63, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Localidade";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(70, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 22);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Descrição";
+            // 
+            // rtDescricao
+            // 
+            this.rtDescricao.Location = new System.Drawing.Point(167, 332);
+            this.rtDescricao.Name = "rtDescricao";
+            this.rtDescricao.Size = new System.Drawing.Size(285, 151);
+            this.rtDescricao.TabIndex = 14;
+            this.rtDescricao.Text = "";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(167, 489);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(285, 41);
+            this.btnCadastrar.TabIndex = 15;
+            this.btnCadastrar.Text = "Cadastrar esta fofura";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(522, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 22);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Poneys cadastrados";
+            // 
+            // dgvPoneys
+            // 
+            this.dgvPoneys.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPoneys.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dgvPoneys.Location = new System.Drawing.Point(522, 49);
+            this.dgvPoneys.Name = "dgvPoneys";
+            this.dgvPoneys.Size = new System.Drawing.Size(744, 483);
+            this.dgvPoneys.TabIndex = 17;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Apelido";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 350;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Raça";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 350;
+            // 
+            // CadastroDeFofuras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1339, 643);
+            this.Controls.Add(this.dgvPoneys);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.rtDescricao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbLocalidade);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbDancarino);
+            this.Controls.Add(this.rbMorto);
+            this.Controls.Add(this.rbVivo);
+            this.Controls.Add(this.cbCorCrista);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbNivelFofura);
+            this.Controls.Add(this.cbRaca);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtApelido);
+            this.Controls.Add(this.label1);
+            this.Name = "CadastroDeFofuras";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tbNivelFofura)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPoneys)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApelido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbRaca;
+        private System.Windows.Forms.TrackBar tbNivelFofura;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbCorCrista;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbVivo;
+        private System.Windows.Forms.RadioButton rbMorto;
+        private System.Windows.Forms.CheckBox cbDancarino;
+        private System.Windows.Forms.ComboBox cbLocalidade;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox rtDescricao;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dgvPoneys;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ GerenciadorDePoneisOO/CadastroDeFofuras.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorDePoneisOO
+{
+    public partial class CadastroDeFofuras : Form
+    {
+        List<Poney> poneys = new List<Poney>();
+        public CadastroDeFofuras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+              if (txtApelido.Text == "")
+            {
+                MessageBox.Show("Digite o apelido");
+                return;
+            }
+
+              if (cbRaca.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione a raca");
+                return;
+            }
+
+            if (cbCorCrista.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione a cor da crista");
+                return;
+            }
+
+            if (rbVivo.Checked == false && rbMorto.Checked == false)
+            {
+                MessageBox.Show("Informe s eesta vivo ou morto");
+                return;
+            }
+
+            if (cbLocalidade.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione a localidade");
+                return;
+            }
+
+           
+            Poney poney = new Poney();
+            poney.Apelido = txtApelido.Text;
+            poney.CorCrista = cbCorCrista.SelectedItem.ToString();
+            poney.Dancarino = cbDancarino.Checked;
+            poney.Localidade = cbLocalidade.SelectedItem.ToString();
+            poney.NivelFofura = tbNivelFofura.Value;
+            poney.VendaVivo = rbVivo.Checked;
+            poney.Descricao = rtDescricao.Text;
+            poney.Raca = cbRaca.SelectedItem.ToString();
+            poneys.Add(poney);
+            dgvPoneys.Rows.Add(new Object[] { poney.Apelido, poney.Raca });
+            LimparCampos();
+
+        }
+
+        private void LimparCampos()
+        {
+            txtApelido.Text = "";
+            rtDescricao.Text = "";
+            cbCorCrista.SelectedIndex = -1;
+            cbLocalidade.SelectedIndex = -1;
+            cbRaca.SelectedIndex = -1;
+            cbDancarino.Checked = false;
+            rbVivo.Checked = false;
+            rbMorto.Checked = false;
+            tbNivelFofura.Value = tbNivelFofura.Minimum;
+        }
+
+       
+    }
+}
